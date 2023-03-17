587e83ecddee1d35b5c471fe7b240ac4a581dc8f
v-kirilov <v_kirilov87@protonmail.ch>
2023-03-17 21:35:20 +0200

    DOM - Task 04

---
+++ Advanced/DOM/Lecture/04.Show More/ShowMore.js
@@ -0,0 +1,7 @@
+function showText() {
+    let spanElement = document.getElementById('text');
+    spanElement.style.display = 'inline';
+
+    let showMoreElement = document.getElementById('more');
+    showMoreElement.style.display = 'none';
+}
